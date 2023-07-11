2e0e446e4c645de11ac6592e9f977dc283849575
chinmayagarwal03 <87461631+chinmayagarwal03@users.noreply.github.com>
2023-07-11 14:50:38 +0530

    implemented middlewares

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
+
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const notFoundMiddleware = require('./middleware/not-found');
+
 const userRoutes = require('./routes/userRoutes');
 const alertRoutes = require('./routes/alertRoutes');
 const sessionRoutes = require('./routes/sessionRoutes');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 app.use('/v1/home/user', userRoutes);
 app.use('/v1/alert',alertRoutes);
 app.use('/v1/session', sessionRoutes);
 
-// Start the server
+app.use(notFoundMiddleware)
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
