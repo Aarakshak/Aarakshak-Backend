97aed67fee4159cca6b68cd1d5109673e929d57d
chinmayagarwal03 <87461631+chinmayagarwal03@users.noreply.github.com>
2023-07-11 13:57:42 +0530

    Current sess-> home->  post request left only

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,6 +9,50 @@ const formatTime = (time) => {
   });
 };
 
+exports.getCurrentSession = async (req, res) => {
+  try {
+    const badgeID = parseInt(req.params.badgeID);
+
+    if (isNaN(badgeID)) {
+      return res.status(400).json({ error: 'Invalid badge ID' });
+    }
+
+    const user = await User.findOne({ badgeID });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const currentDate = new Date().toISOString().split('T')[0];
+
+    const currentSession = user.sessions.find(({ session }) => {
+      const sessionDate = session.sessionDate.toISOString().split('T')[0];
+      return sessionDate === currentDate;
+    });
+
+    if (!currentSession) {
+      return res.status(404).json({ error: 'No current session found' });
+    }
+
+    const { sessionLocation, sessionLocation2, startTime, endTime, checkpoints } = currentSession.session;
+
+    const response = {
+      location1: sessionLocation,
+      location2: sessionLocation2,
+      reportingTo: user.reportsTo,
+      checkInTime: startTime,
+      checkOutTime: endTime,
+      checkpointCount: checkpoints.length,
+      checkpoints,
+      date: currentDate,
+    };
+
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
 
 exports.getUserByBadgeID = async (req, res) => {
   try {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/:badgeID', userController.getUserByBadgeID);
+router.get('/:badgeID/current-session', userController.getCurrentSession);
 
 module.exports = router;
