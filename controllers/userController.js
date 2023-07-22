9ade4aed8d8c2871fbe0f55ef4bfe7926da4295a
chinmayagarwal03 <87461631+chinmayagarwal03@users.noreply.github.com>
2023-07-22 16:37:08 +0530

    otp-validation, imageurl call done

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,20 +29,18 @@ function generateOTP() {
 async function sendOTPByEmail(email, otp)  {
   
   const transporter = nodemailer.createTransport({
-    host: "smtp.forwardemail.net",
-    port: 465,
-    secure: true,
-    auth: {
-      user: 'XXXXX',
-      pass: 'XXXXX'
-    },
-  });
+    service :'gmail',
+    auth : {
+      user: 'cagarwal_be20@thapar.edu',
+      pass: 'ekeyrdwzoxybcsnx'
+    }
+});
 
   const mailOptions = {
-    from : 'XXXXX',
+    from : 'cagarwal_be20@thapar.edu',
     to : email,
     subject: 'OTP Verification',
-    text : `Your OTP is: ${otp}, `
+    text : `Your OTP is: ${otp}`
   };
   try{
     await transporter.sendMail(mailOptions);
@@ -65,20 +63,41 @@ exports.loginUser = async (req, res) => {
     if (password !== user.password) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-
-    if(otp) {
-      if(user.otp !== otp || Date.now() > user.otpExpiration){
-        return res.status(401).json({ error: 'Invalid OTP' });
-      }
-      user.otp = undefined;
-      user.otpExpiration = undefined;
-    }else {
       const newOTP = generateOTP();
       user.otp = newOTP;
       user.otpExpiration = Date.now() + 5 * 60 * 1000;
 
       await sendOTPByEmail(emailId, newOTP);
+    
+      user = await user.save();
+
+    // Return the token and badgeID to the client
+    res.json({ message: 'OTP sent successfully', badgeID: user.badgeID });
+  }catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+exports.verifyOTP = async (req, res) => {
+  try {
+    let {badgeID, otp} = req.body;
+    let user = await User.findOne({badgeID});
+    if(!user)
+    {
+      return res.status(404).json({error : 'User not found'})
     }
+
+    if(user.otp !== otp || Date.now() > user.otpExpiration)
+    {
+      return res.status(401).json({error: 'Invalid otp'});
+    }
+
+    const imageUrl = user.profilePic; 
+
+    user.otp = undefined;
+    user.otpExpiration = undefined;
+
     user = await user.save();
     const token = jwt.sign(
       { emailId: user.emailId, badgeID: user.badgeID }, // Include badgeID in the payload
@@ -87,19 +106,14 @@ exports.loginUser = async (req, res) => {
         expiresIn: '1h', // Set the expiration time for the token
       }
     );
-
     user.jwtToken = token;
     await user.save();
 
-  
-
-
-    // Return the token and badgeID to the client
-    res.json({ token, badgeID: user.badgeID });
+    res.json({ token, badgeID: user.badgeID , imageUrl});
   }catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Server error' });
-  }
+}
 };
 
 exports.createIssue = async (req, res) => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/login', userController.loginUser);
+router.post('/verify-otp', userController.verifyOTP);
 router.get('/:badgeID', userController.getUserByBadgeID);
 router.get('/:badgeID/current-session', userController.getCurrentSession);
 router.post('/:badgeID/issues', userController.createIssue);
